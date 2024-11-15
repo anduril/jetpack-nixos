2e15c66727b2938bd496e6a62fa60e618ac94ac7
Jared Baur <jaredbaur@fastmail.com>
2024-11-14 20:28:08 -0800

    optee: add gen_ekb.py script
    The gen_ekb.py script from the nv-optee source is a useful utility for
    generating EKB images.

---
+++ pkgs/optee/gen-ekb.nix
@@ -0,0 +1,21 @@
+{ gitRepos, l4tVersion, stdenv, python3 }:
+
+stdenv.mkDerivation {
+  pname = "gen_ekb.py";
+  src = gitRepos."tegra/optee-src/nv-optee";
+  version = l4tVersion;
+  dontBuild = true;
+  buildInputs = [
+    (python3.withPackages (p: with p; [
+      cryptography
+      pycryptodome
+    ]))
+  ];
+  installPhase = ''
+    runHook preInstall
+    install -D optee/samples/hwkey-agent/host/tool/gen_ekb/gen_ekb.py \
+      $out/bin/gen_ekb.py
+    patchShebangs --host $out/bin/gen_ekb.py
+    runHook postInstall
+  '';
+}
