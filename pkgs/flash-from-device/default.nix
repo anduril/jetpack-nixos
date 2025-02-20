85d66dbc01fa7ee1dae31c5b184836f23f5a5327
Daniel Fullmer <dfullmer@anduril.com>
2025-02-20 09:52:47 -0800

    flash-from-device: workaround build failure for mtdutils

--- pkgs/flash-from-device/default.nix
+++ pkgs/flash-from-device/default.nix
@@ -1,11 +1,20 @@
 { lib, pkgsStatic, runCommand, tegra-eeprom-tool-static }:
 
 let
+  # Workaround build failure with pkgsStatic.mtdutils in NixOS 24.11
+  # > configure: WARNING: cannot find CMocka library required for unit tests
+  # > configure: unit tests can optionally be disabled
+  # > configure: error: missing one or more dependencies
+  mtdutils = pkgsStatic.mtdutils.overrideAttrs (_: {
+    configureFlags = [ ];
+    doCheck = false;
+  });
+
   # Make the package smaller so it doesn't blow up the initrd size
   staticDeps = runCommand "static-deps" { } ''
     mkdir -p $out/bin
-    cp ${pkgsStatic.mtdutils}/bin/mtd_debug $out/bin
-    cp ${pkgsStatic.mtdutils}/bin/flash_erase $out/bin
+    cp ${mtdutils}/bin/mtd_debug $out/bin
+    cp ${mtdutils}/bin/flash_erase $out/bin
     cp ${tegra-eeprom-tool-static}/bin/tegra-boardspec $out/bin
   '';
   name = "flash-from-device";
