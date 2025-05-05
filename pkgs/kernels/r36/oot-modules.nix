fdd72c7bde24c9dd7e47056cd30ec068ddc94926
Elliot Berman <eberman@anduril.com>
2025-05-05 13:21:37 -0700

    Add l4t r36.4.3 kernel derivation
    Introduce new r36 kernel derivation based on L4T r36.4.3. Subsequent
    commits will wire this up into a buildable derivation.
    
    Signed-off-by: Elliot Berman <eberman@anduril.com>

---
+++ pkgs/kernels/r36/oot-modules.nix
@@ -0,0 +1,94 @@
+{ applyPatches
+, bspSrc
+, buildPackages
+, gitRepos
+, kernel
+, l4tMajorMinorPatchVersion
+, lib
+, runCommand
+, stdenv
+, ...
+}:
+let
+  patchedBsp = applyPatches {
+    name = "patchedBsp";
+    src = bspSrc;
+    patches = [
+      ./Makefile.diff
+    ];
+  };
+
+  mkCopyProjectCommand = project: ''
+    mkdir -p "$out/${project.name}"
+    cp --no-preserve=all -vr "${project}"/. "$out/${project.name}"
+  '';
+
+  l4t-oot-projects = [
+    (gitRepos.hwpm.overrideAttrs { name = "hwpm"; })
+    (applyPatches {
+      name = "nvidia-oot";
+      src = gitRepos.nvidia-oot;
+      patches = [
+        ./0001-rtl8822ce-Fix-Werror-address.patch
+        ./0002-sound-Fix-include-path-for-tegra-virt-alt-include.patch
+      ];
+    })
+    (gitRepos.nvgpu.overrideAttrs { name = "nvgpu"; })
+    (applyPatches {
+      name = "nvdisplay";
+      src = gitRepos.nvdisplay;
+      patches = [
+        ./0001-nvidia-drm-Guard-nv_dev-in-nv_drm_suspend_resume.patch
+        ./0002-ANDURIL-Add-some-missing-BASE_CFLAGS.patch
+        ./0003-ANDURIL-Update-drm_gem_object_vmap_has_map_arg-test.patch
+      ];
+    })
+    (applyPatches {
+      name = "nvethernetrm";
+      src = gitRepos.nvethernetrm;
+      # Some directories in the git repo are RO.
+      # This works for L4T b/c they use different output directory
+      postPatch = ''
+        chmod -R u+w osi
+      '';
+    })
+  ];
+
+  l4t-oot-modules-sources = runCommand "l4t-oot-sources" { }
+    (
+      # Copy the Makefile
+      ''
+        mkdir -p "$out"
+        cp "${patchedBsp}/source/Makefile" "$out/Makefile"
+      ''
+      # copy the projects
+      + lib.strings.concatMapStringsSep "\n" mkCopyProjectCommand l4t-oot-projects
+      # See bspSrc/source/source_sync.sh symlink at end of file
+      + ''
+        ln -vsrf "$out/nvethernetrm" "$out/nvidia-oot/drivers/net/ethernet/nvidia/nvethernet/nvethernetrm"
+      ''
+    );
+in
+stdenv.mkDerivation (finalAttrs: {
+  pname = "l4t-oot-modules";
+  version = "${l4tMajorMinorPatchVersion}";
+  src = l4t-oot-modules-sources;
+
+  inherit kernel;
+
+  nativeBuildInputs = finalAttrs.kernel.moduleBuildDependencies;
+  depsBuildBuild = [ buildPackages.stdenv.cc ];
+
+  # See bspSrc/source/Makefile
+  makeFlags = finalAttrs.kernel.makeFlags ++ [
+    "KERNEL_HEADERS=${finalAttrs.kernel.dev}/lib/modules/${finalAttrs.kernel.modDirVersion}/source"
+    "KERNEL_OUTPUT=${finalAttrs.kernel.dev}/lib/modules/${finalAttrs.kernel.modDirVersion}/build"
+    "INSTALL_MOD_PATH=$(out)"
+  ];
+
+  # # GCC 14.2 seems confused about DRM_MODESET_LOCK_ALL_BEGIN/DRM_MODESET_LOCK_ALL_END in nvdisplay/kernel-open/nvidia-drm/nvidia-drm-drv.c:1344
+  # extraMakeFlags = [ "KCFLAGS=-Wno-error=unused-label" ];
+
+  buildFlags = [ "modules" ];
+  installTargets = [ "modules_install" ];
+})
