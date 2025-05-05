328857624867aa000dd5bc7cdfb1273c06841531
Elliot Berman <eberman@anduril.com>
2025-05-05 09:32:00 -0700

    containers: Version l4t.json
    Rename l4t.json to r35.6-l4t.json.

--- pkgs/containers/deps.nix
+++ pkgs/containers/deps.nix
@@ -2,6 +2,7 @@
 , runCommand
 , dpkg
 , debs
+, l4tVersion
 }:
 
 runCommand "container-deps" { nativeBuildInputs = [ dpkg ]; }
@@ -21,4 +22,4 @@ runCommand "container-deps" { nativeBuildInputs = [ dpkg ]; }
           fi
         '')
         debFiles)))
-      (lib.importJSON ./l4t.json)))
+      (lib.importJSON ./r${lib.versions.majorMinor l4tVersion}-l4t.json)))
