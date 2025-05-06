0883acc10d911dc4adf5847f83b32fa94e016dc5
Elliot Berman <eberman@anduril.com>
2025-05-05 17:58:39 -0700

    l4t: l4t-cuda: libnvidia-ptxjitcompiler.so uses cuda driver version
    In jetpack 6, libnvidia-ptxjitcompiler.so is suffixed with the CUDA
    driver version, not the l4t version. Describe the CUDA driver version in
    the overlay.nix and use it, if set.

--- jetpack5-overlay.nix
+++ jetpack5-overlay.nix
@@ -4,5 +4,9 @@ import ./mk-overlay.nix {
   l4tMajorMinorPatchVersion = "35.6.1";
   cudaMajorMinorPatchVersion = "11.4.298";
 
+  # used to version libnvidia-ptxjitcompiler.so. L4T r35 uses l4tMajorMinorPatchVersion, so set to null
+  # and we'll use l4tMajorMinorPatchVersion instead
+  cudaDriverMajorMinorVersion = null;
+
   bspHash = "sha256-nqKEd3R7MJXuec3Q4odDJ9SNTUD1FyluWg/SeeptbUE=";
 }
--- jetpack6-overlay.nix
+++ jetpack6-overlay.nix
@@ -4,5 +4,9 @@ import ./mk-overlay.nix {
   l4tMajorMinorPatchVersion = "36.4.3";
   cudaMajorMinorPatchVersion = "12.6.10";
 
+  # nix build .#legacyPacakges.l4t-cuda.src; unpack the deb; find libnvidia-ptxjijtcompiler.so
+  # and use that.
+  cudaDriverMajorMinorVersion = "540.4.0";
+
   bspHash = "sha256-lJpEBJxM5qjv31cuoIIMh09u5dQco+STW58OONEYc9I=";
 }
