73bc0820a916526ce8e305b61def89b27fde40c6
Elliot Berman <eberman@anduril.com>
2025-08-07 16:01:27 -0700

    initrdFlashScript: Force USB to peripheral mode
    Carrier boards are not required to support USB OTG mode. Ensure the mode
    of the USB port is peripheral. Deices  must support peripheral mode,
    else one would not be able to flash it. This only applies to the
    recovery mode boot, and not to the images that are being flashed.

--- device-pkgs/initrdflash-script.nix
+++ device-pkgs/initrdflash-script.nix
@@ -2,7 +2,9 @@
 , config
 , flashInitrd
 , lib
+, l4tMajorMinorPatchVersion
 , writeText
+, deviceTree
 , tio
 , expect
 ,
@@ -12,11 +14,82 @@ let
   inherit (flashInitrd.passthru) manufacturer product serialnumber;
   serialPortId = "usb-${manufacturer}_${product}_${serialnumber}-if00";
 
+
+  forceXusbPeripheralDts =
+    let
+      overridePaths = {
+        "36" = {
+          orin = {
+            xudcPadctlPath = "bus@0/padctl@3520000";
+            xudcPath = "bus@0/usb@3550000";
+          };
+        };
+        "35" = {
+          orin = {
+            xudcPadctlPath = "xusb_padctl@3520000";
+            xudcPath = "xudc@3550000";
+          };
+          xavier = {
+            xudcPadctlPath = "xusb_padctl@3520000";
+            xudcPath = "xudc@3550000";
+          };
+        };
+      };
+      l4tMajor = lib.versions.major l4tMajorMinorPatchVersion;
+      soc = builtins.elemAt (lib.strings.split "-" cfg.som) 0;
+      inherit (overridePaths.${l4tMajor}.${soc}) xudcPadctlPath xudcPath;
+    in
+    writeText "dts" ''
+      /dts-v1/;
+
+      / {
+        fragment@0 {
+          target-path = "/${xudcPadctlPath}/ports/usb2-0";
+
+          board_config {
+            sw-modules = "kernel", "uefi";
+          };
+
+          __overlay__ {
+            mode = "peripheral";
+            /* usb-role-switch and connector are required
+              * use a dummy connector that claims USB is connected at boot
+              */
+            usb-role-switch;
+            connector {
+              compatible = "usb-b-connector", "gpio-usb-b-connector";
+              label = "usb-recovery";
+              cable-connected-on-boot = <2>;
+            };
+          };
+        };
+
+        fragment@1 {
+          target-path = "/${xudcPath}";
+
+          board_config {
+            sw-modules = "kernel", "uefi";
+          };
+
+          __overlay__ {
+            status = "okay";
+          };
+        };
+      };
+    '';
+  forceXusbPeripheralDtbo = deviceTree.compileDTS {
+    # We force USB port to peripheral mode, in case the carrier board doesn't support OTG
+    # Fortunately, the USB controller is all in the SoC, so we can override it directly here
+    name = "force-xusb-peripheral.dtbo";
+    dtsFile = forceXusbPeripheralDts;
+  };
+
   rcmScript = mkRcmBootScript {
     kernelPath = "${config.system.build.kernel}/${config.system.boot.loader.kernelFile}";
     initrdPath = "${flashInitrd}/initrd";
     kernelCmdline = lib.concatStringsSep " " [
-      "console=ttyTCU0,115200" "sdhci_tegra.en_boot_part_access=1"
+      "console=ttyTCU0,115200"
+      "sdhci_tegra.en_boot_part_access=1"
     ];
     # During the initrd flash script, we upload two edk2 builds to the
     # board, one that is only used temporarily to boot into our
@@ -30,7 +103,7 @@ let
     # configured in nix) in there and is not affected dynamically during
     # the flashing procedure. NVIDIA ensures that when the device is
     # using RCM boot, only the boot mode named "boot.img" is used (see https://gist.github.com/jmbaur/1ca79436e69eadc0a38ec0b43b16cb2f#file-flash-sh-L1675).
-    additionalDtbOverlays = lib.filter (path: (path.name or "") != "DefaultBootOrder.dtbo") cfg.flashScriptOverrides.additionalDtbOverlays;
+    additionalDtbOverlays = (lib.filter (path: (path.name or "") != "DefaultBootOrder.dtbo") cfg.flashScriptOverrides.additionalDtbOverlays) ++ [ forceXusbPeripheralDtbo ];
   };
 
   expectScript = writeText "expect-initrd-flash" ''
