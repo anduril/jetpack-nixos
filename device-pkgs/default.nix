a7c3fb8658955ea6b27fbc5a63d562c7ae4cffb0
Elliot Berman <eberman@anduril.com>
2025-08-06 18:41:41 -0700

    initrdFlashScript: Connect to the new serial port
    Show the output from the new serial port that reports the progress and
    check to see if it prints the flashing succeeded message.

--- device-pkgs/default.nix
+++ device-pkgs/default.nix
@@ -11,6 +11,7 @@
 , nvidia-jetpack
 , writeShellApplication
 , buildPackages
+, picocom
 }:
 
 let
@@ -118,31 +119,65 @@ let
   initrdFlashScript =
     writeShellApplication {
       name = "initrd-flash-${hostName}";
-      text = ''
-        ${mkRcmBootScript {
-          kernelPath = "${config.system.build.kernel}/${config.system.boot.loader.kernelFile}";
-          initrdPath = "${flashInitrd}/initrd";
-          kernelCmdline = lib.concatStringsSep " " [
-            "console=ttyTCU0,115200" "sdhci_tegra.en_boot_part_access=1"
-          ];
-          # During the initrd flash script, we upload two edk2 builds to the
-          # board, one that is only used temporarily to boot into our
-          # kernel/initrd to perform the flashing, and another one that is
-          # persisted to the firmware storage medium for future booting. We
-          # don't want to influence the boot order of the temporary edk2 since
-          # this may cause that edk2 to boot from something other than our
-          # intended flashing kernel/initrd combo (e.g. disk or network). Since
-          # the edk2 build that we actually persist to the board is embedded in
-          # the initrd used for flashing, we have the desired boot order (as
-          # configured in nix) in there and is not affected dynamically during
-          # the flashing procedure. NVIDIA ensures that when the device is
-          # using RCM boot, only the boot mode named "boot.img" is used (see https://gist.github.com/jmbaur/1ca79436e69eadc0a38ec0b43b16cb2f#file-flash-sh-L1675).
-          additionalDtbOverlays = lib.filter (path: (path.name or "") != "DefaultBootOrder.dtbo") cfg.flashScriptOverrides.additionalDtbOverlays;
-        }}
-        echo
-        echo "Jetson device should now be flashing and will reboot when complete."
-        echo "You may watch the progress of this on the device's serial port"
-      '';
+      runtimeInputs = [ picocom ];
+      text =
+        let
+          inherit (flashInitrd.passthru) manufacturer product serialnumber;
+          serialPortId = "usb-${manufacturer}_${product}_${serialnumber}-if00";
+        in
+        ''
+          ${mkRcmBootScript {
+            kernelPath = "${config.system.build.kernel}/${config.system.boot.loader.kernelFile}";
+            initrdPath = "${flashInitrd}/initrd";
+            kernelCmdline = lib.concatStringsSep " " [
+              "console=ttyTCU0,115200" "sdhci_tegra.en_boot_part_access=1"
+            ];
+            # During the initrd flash script, we upload two edk2 builds to the
+            # board, one that is only used temporarily to boot into our
+            # kernel/initrd to perform the flashing, and another one that is
+            # persisted to the firmware storage medium for future booting. We
+            # don't want to influence the boot order of the temporary edk2 since
+            # this may cause that edk2 to boot from something other than our
+            # intended flashing kernel/initrd combo (e.g. disk or network). Since
+            # the edk2 build that we actually persist to the board is embedded in
+            # the initrd used for flashing, we have the desired boot order (as
+            # configured in nix) in there and is not affected dynamically during
+            # the flashing procedure. NVIDIA ensures that when the device is
+            # using RCM boot, only the boot mode named "boot.img" is used (see https://gist.github.com/jmbaur/1ca79436e69eadc0a38ec0b43b16cb2f#file-flash-sh-L1675).
+            additionalDtbOverlays = lib.filter (path: (path.name or "") != "DefaultBootOrder.dtbo") cfg.flashScriptOverrides.additionalDtbOverlays;
+          }}
+          echo
+          echo "Jetson device should now be flashing and will reboot when complete."
+
+          # TODO(eberman): maybe stop assuming /dev/serial/by-id is present
+          # It'll be populated with udev, so most Linux systems will work fine
+          echo -n "Waiting for Jetson device to appear via USB, this may take up to 4 minutes"
+
+          counter=0
+          until [[ -e /dev/serial/by-id/${serialPortId} || $counter -gt ${builtins.toString (4 * 60 * 2)} ]] ; do
+            echo -n "."
+            sleep 0.5
+            ((++counter))
+          done
+          echo
+
+          if [ ! -e /dev/serial/by-id/${serialPortId} ] ; then
+            echo "Jetson device not detected, please monitor the serial port or wait 5-15 minutes"
+            exit 2
+          fi
+
+          logs=$(mktemp)
+          echo "^a^x to kill the terminal"
+          picocom --quiet --logfile "$logs" /dev/serial/by-id/${serialPortId} || true
+
+          if ! grep -q "Flashing platform firmware successful" "$logs" ; then
+            rm "$logs"
+            echo "Flashing may have failed"
+            exit 1
+          fi
+          rm "$logs"
+          echo "Device reported flashing succeeded. Device should be rebooting."
+        '';
       meta.platforms = [ "x86_64-linux" ];
     };
 
