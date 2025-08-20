039f1d564ad7682dc61edd4fce4b078d1a37a62b
Elliot Berman <eberman@anduril.com>
2025-08-20 10:41:21 -0700

    Rename ota_helpers.func to ota_helpers.sh

--- pkgs/ota-utils/default.nix
+++ pkgs/ota-utils/default.nix
@@ -21,16 +21,16 @@ stdenvNoCC.mkDerivation {
     cp ${./ota-apply-capsule-update.sh} $out/bin/ota-apply-capsule-update
     cp ${./ota-check-firmware.sh} $out/bin/ota-check-firmware
     cp ${./ota-abort-capsule-update.sh} $out/bin/ota-abort-capsule-update
-    cp ${./ota_helpers.func} $out/share/ota_helpers.func
+    cp ${./ota_helpers.sh} $out/share/ota_helpers.sh
     chmod +x $out/bin/ota-setup-efivars $out/bin/ota-apply-capsule-update $out/bin/ota-check-firmware $out/bin/ota-abort-capsule-update
 
-    for path in $out/bin/ota-apply-capsule-update $out/share/ota_helpers.func $out/bin/ota-abort-capsule-update; do
+    for path in $out/bin/ota-apply-capsule-update $out/share/ota_helpers.sh $out/bin/ota-abort-capsule-update; do
       substituteInPlace "$path" --subst-var efiSysMountPoint
     done
 
     for fname in ota-setup-efivars ota-apply-capsule-update ota-abort-capsule-update; do
       substituteInPlace $out/bin/$fname \
-        --replace "@ota_helpers@" "$out/share/ota_helpers.func"
+        --replace "@ota_helpers@" "$out/share/ota_helpers.sh"
       sed -i '2a export PATH=${lib.makeBinPath [ util-linux e2fsprogs tegra-eeprom-tool ]}:$PATH' $out/bin/$fname
     done
 
