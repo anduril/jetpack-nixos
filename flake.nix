fab79fe00d96f4db03198656da666cb5474a6d9c
Tanel Dettenborn <tanel.dettenborn@tii.ae>
2025-08-26 16:03:35 +0300

    Orin NX: Add support for xavier NX carrier board
    Signed-off-by: Tanel Dettenborn <tanel.dettenborn@tii.ae>

--- flake.nix
+++ flake.nix
@@ -55,6 +55,7 @@
         { som = "orin-agx"; carrierBoard = "devkit"; }
         { som = "orin-agx-industrial"; carrierBoard = "devkit"; }
         { som = "orin-nx"; carrierBoard = "devkit"; }
+        { som = "orin-nx"; carrierBoard = "xavierNxDevkit"; }
         { som = "orin-nano"; carrierBoard = "devkit"; }
         { som = "orin-nx"; carrierBoard = "devkit"; super = true; }
         { som = "orin-nano"; carrierBoard = "devkit"; super = true; }
@@ -63,6 +64,7 @@
         { som = "orin-nx"; carrierBoard = "devkit"; majorVersion = "5"; }
         { som = "orin-nano"; carrierBoard = "devkit"; majorVersion = "5"; }
         { som = "orin-nx"; carrierBoard = "devkit"; super = true; majorVersion = "5"; }
+        { som = "orin-nx"; carrierBoard = "xavierNxDevkit"; majorVersion = "5"; }
         { som = "orin-nano"; carrierBoard = "devkit"; super = true; majorVersion = "5"; }
         { som = "thor-agx"; carrierBoard = "devkit"; }
         { som = "xavier-agx"; carrierBoard = "devkit"; }
