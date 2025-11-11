a080cf23a045293a5544e1a347aefc4ff12ddb9c
Elliot Berman <eberman@anduril.com>
2025-11-11 13:29:29 -0800

    cuda-samples: Use cudaPackages.cuda-samples for CUDA >=13
    We can use nixpkgs' version of cuda-samples starting with CUDA 13.

--- pkgs/samples/cuda-test.nix
+++ pkgs/samples/cuda-test.nix
@@ -1,10 +1,15 @@
-{ cuda-samples, writeShellApplication }:
+{ cuda-samples
+, cudaPackages
+, lib
+, writeShellApplication
+}:
 writeShellApplication {
   name = "cuda-test";
   text = ''
     BINARIES=(
-      deviceQuery deviceQueryDrv bandwidthTest clock clock_nvrtc
+      deviceQuery deviceQueryDrv clock clock_nvrtc
       matrixMul matrixMulCUBLAS matrixMulDrv matrixMulDynlinkJIT
+      ${lib.optionalString (cudaPackages.cudaOlder "13") "bandwidthTest"}
     )
     for binary in "''${BINARIES[@]}"; do
       real="$(find ${cuda-samples} -type f -name "$binary")"
