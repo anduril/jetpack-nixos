311b41252cf6f9c722dabc28242c2bdb6dbc5c11
Elliot Berman <eberman@anduril.com>
2025-11-11 13:31:34 -0800

    cupti-samples: Updates for CUDA 13
    In CUDA 13, we must specify the CUPTI_INSTALL_PATH explictly. Don't
    build openacc_trace as it requires nvc++, which is not currently
    packaged. Also, cudaVersionDashes is specific to nvidia-jetpack's
    cudaPackages.

--- pkgs/samples/cupti-test.nix
+++ pkgs/samples/cupti-test.nix
@@ -1,4 +1,9 @@
-{ cupti-samples, writeShellApplication }:
+{ cupti-samples
+, l4tOlder
+, cudaPackages
+, lib
+, writeShellApplication
+}:
 # NOTE: Must run as root:
 # auto_range_profiling.cu:336: error: function cuptiProfilerInitialize(&profilerInitializeParams) failed with error
 # CUPTI_ERROR_INSUFFICIENT_PRIVILEGES.
@@ -7,14 +12,17 @@ writeShellApplication {
   text = ''
     # Not entirely sure which utilities are relevant here, I'll just pick a few
     # See: https://docs.nvidia.com/cupti/main/main.html?highlight=samples#samples
-    #
-    for binary in auto_range_profiling callback_timestamp pc_sampling; do
+    BINARIES=(
+      auto_range_profiling callback_timestamp
+      ${lib.optionalString (cudaPackages.cudaOlder "13") "pc_sampling"}
+    )
+    for binary in "''${BINARIES[@]}"; do
       echo " * Running $binary"
       ${cupti-samples}/bin/"$binary"
       echo
       echo
     done
-
+  '' + lib.optionalString (l4tOlder "36") ''
     # cupti_query fails on Orin with the following message:
     # "Error CUPTI_ERROR_LEGACY_PROFILER_NOT_SUPPORTED for CUPTI API function 'cuptiDeviceEnumEventDomains'."
     #
