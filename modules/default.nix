anduril/jetpack-nixos

commit bc71d9a96735b982e09b33053d1accbf4b9930ad

Add thor firmware

modules/default.nix
@@ -375,8 +375,9 @@ in
 
       hardware.firmware = with pkgs.nvidia-jetpack; [
         l4t-firmware
-        l4t-xusb-firmware # usb firmware also present in linux-firmware package, but that package is huge and has much more than needed
         cudaPackages.vpi-firmware # Optional, but needed for pva_auth_allowlist firmware file used by VPI2
+      ] ++ lib.optionals (l4tOlder "36") [
+        l4t-xusb-firmware # usb firmware also present in linux-firmware package, but that package is huge and has much more than needed
       ];
 
       hardware.deviceTree.enable = true;

modules/devices.nix
@@ -210,5 +210,21 @@ in
           "rtw88/rtw8822c_wow_fw.bin"
         ])
       ];
+    })
+    (lib.mkIf (cfg.som == "thor-agx") {
+      hardware.firmware = [
+        (extractLinuxFirmware "xusb-firmware" ([ "nvidia/tegra186/xusb.bin" ] ++ lib.optionals
+          (config.hardware.bluetooth.enable
+            || config.networking.wireless.enable
+            || config.networking.wireless.iwd.enable) [
+          "rtl_nic/rtl8153a-2.fw"
+          "rtl_nic/rtl8153a-3.fw"
+          "rtl_nic/rtl8153a-4.fw"
+          "rtl_nic/rtl8153b-2.fw"
+          "rtl_nic/rtl8153c-1.fw"
+          "rtl_nic/rtl8156a-2.fw"
+          "rtl_nic/rtl8156b-2.fw"
+        ]))
+      ];
     })]);
 }

commit d40da21f746e17b0a932b24e39cf27fa732c529f

module: Fix l4t-xusb-firmware inclusion in JetPack 6
It should have be applicable to JetPack 5 and JetPack 6.

Fixes: bc71d9a96735 ("Add thor firmware")

modules/default.nix
@@ -389,7 +389,7 @@ in
         l4t-firmware
       ] ++ lib.optionals pkgs.config.cudaSupport [
         cudaPackages.vpi-firmware # Optional, but needed for pva_auth_allowlist firmware file used by VPI2
-      ] ++ lib.optionals (l4tOlder "36") [
+      ] ++ lib.optionals (l4tOlder "38") [
         l4t-xusb-firmware # usb firmware also present in linux-firmware package, but that package is huge and has much more than needed
       ];
 