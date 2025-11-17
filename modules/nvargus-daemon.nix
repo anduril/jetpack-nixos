068a6c816bd9fd35e8076aeeedd1ef1ce8917039
Daniel Fullmer <dfullmer@anduril.com>
2025-11-17 13:22:43 -0800

    Move libegl symlink hack to nvargus-daemon.nix

--- modules/nvargus-daemon.nix
+++ modules/nvargus-daemon.nix
@@ -72,5 +72,38 @@ in
         };
         wantedBy = [ "multi-user.target" ];
       };
+
+    # For some unknown reason, the libnvscf.so library has a dlopen call to a hard path:
+    # `/usr/lib/aarch64-linux-gnu/tegra-egl/libEGL_nvidia.so.0`
+    # This causes loading errors for libargus applications and the nvargus-daemon.
+    # Errors will look like this:
+    # SCF: Error NotSupported: Failed to load EGL library
+    # To fix this, create a symlink to the correct EGL library in the above directory.
+    #
+    # An alternative approach would be to wrap the library with an LD_PRELOAD to a dlopen call
+    # that replaces the hardcoded path with the correct path.
+    # However, since dynamic library symbol lookups start with the calling binary,
+    # this override would have to happen at the binary level, which means every binary
+    # would need to be wrapped. This is less desirable than simply adding the following symlink.
+    # TODO: Replace with systemd-tmpfiles?
+    systemd.services.create-libegl-symlink =
+      let
+        linkEglLib = pkgs.writeShellScriptBin "link-egl-lib" ''
+          ${lib.getExe' pkgs.coreutils "mkdir"} -p /usr/lib/aarch64-linux-gnu/tegra-egl
+          ${lib.getExe' pkgs.coreutils "ln"} -s /run/opengl-driver/lib/libEGL_nvidia.so.0 /usr/lib/aarch64-linux-gnu/tegra-egl/libEGL_nvidia.so.0
+        '';
+      in
+      {
+        enable = cfg.configureCuda;
+        description = "Create a symlink for libEGL_nvidia.so.0 at /usr/lib/aarch64-linux-gnu/tegra-egl/";
+        unitConfig = {
+          ConditionPathExists = "!/usr/lib/aarch64-linux-gnu/tegra-egl/libEGL_nvidia.so.0";
+        };
+        serviceConfig = {
+          type = "oneshot";
+          ExecStart = lib.getExe linkEglLib;
+        };
+        wantedBy = [ "multi-user.target" ];
+      };
   };
 }
