anduril/jetpack-nixos

commit d4efffc357e0e841b6f450d0b218dce375409882

cuda: Introduce L4T cuda-extensions
We have converged towards the upstream API for creating cudaPackages.
JetPack 7 will use the nixpkgs' cudaPackages_13. There are a few
packages which are still L4T specific. Namely: vpi and vpi-firmware.
Move these into a separate overlay (extension) which will be applied
when using upstream nixpkgs' cudaPackages.

overlay.nix
@@ -138,10 +138,12 @@ in
 
   cudaPackages = final.cudaPackages_11;
 
-  # Update _cuda's database with an entry allowing Orin on CUDA 11.4.
-  # NOTE: This can be removed when the minimum supported Nixpkgs version is 25.11,
-  # since the CUDA db will contain these fixes.
-  _cuda = prev._cuda.extend (final: prev: recursiveUpdate prev {
+  _cuda = prev._cuda.extend (_: prev: recursiveUpdate prev {
+    extensions = prev.extensions ++ [ (final.callPackage ./pkgs/cuda-extensions { }) ];
+
+    # Update _cuda's database with an entry allowing Orin on CUDA 11.4.
+    # NOTE: This can be removed when the minimum supported Nixpkgs version is 25.11,
+    # since the CUDA db will contain these fixes.
     bootstrapData.cudaCapabilityToInfo = {
       "7.2" = {
         archName = "Volta";

mk-overlay.nix
@@ -233,13 +233,16 @@ makeScope final.newScope (self: {
     # NOTE: We must ensure the scope allows us to draw on the contents of nvidia-jetpack.
     makeScope pkgs'.nvidia-jetpack.newScope (
       extends
-        # Add the packages built from debians
-        # NOTE: We do not extend with _cuda.extensions because the JetPack CUDA package sets have a different set of
-        # attributes compared to upstream; we should not expect the overlays provided to work with our package set.
-        (finalCudaPackages: _: packagesFromDirectoryRecursive {
-          directory = ./pkgs/cuda-packages;
-          inherit (finalCudaPackages) callPackage;
-        })
+        (composeManyExtensions [
+          # Add the packages built from debians
+          # NOTE: We do not extend with _cuda.extensions because the JetPack CUDA package sets have a different set of
+          # attributes compared to upstream; we should not expect the overlays provided to work with our package set.
+          (finalCudaPackages: _: packagesFromDirectoryRecursive {
+            directory = ./pkgs/cuda-packages;
+            inherit (finalCudaPackages) callPackage;
+          })
+          (final.callPackage ./pkgs/cuda-extensions { })
+        ])
         passthruFunction
     );
 

pkgs/cuda-extensions/default.nix
@@ -1,0 +1,13 @@
+{ lib
+,
+}:
+finalAttrs: prevAttrs:
+let
+  packages = lib.packagesFromDirectoryRecursive {
+    inherit (finalAttrs) callPackage;
+    directory = ./.;
+  };
+in
+{
+  inherit (packages) vpi vpi-firmware;
+}

pkgs/cuda-extensions/vpi-firmware.nix
@@ -1,17 +1,18 @@
 # Needed for vpi${majorVersion}-samples benchmark w/ pva to work
-{ debs
-, dpkg
+{ dpkg
 , runCommand
-, l4tMajorMinorPatchVersion
+, nvidia-jetpack
 , lib
 ,
 }:
 let
-  majorVersion = {
+  inherit (nvidia-jetpack) l4tMajorMinorPatchVersion debs;
+
+  majorVersion = lib.getAttr (lib.versions.major l4tMajorMinorPatchVersion) {
     "35" = "2";
     "36" = "3";
     "38" = "4";
-  }.${lib.versions.major l4tMajorMinorPatchVersion};
+  };
 in
 runCommand "vpi${majorVersion}-firmware" { nativeBuildInputs = [ dpkg ]; } ''
   dpkg-deb -x ${debs.common."libnvvpi${majorVersion}".src} source

pkgs/cuda-extensions/vpi/package.nix
@@ -1,20 +1,24 @@
-{ buildFromDebs
-, debs
-, l4t-3d-core
-, l4t-core
-, l4t-cupva
-, l4t-multimedia
-, libcufft
+{ libcufft
 , libnpp
-, l4tMajorMinorPatchVersion
 , lib
+, nvidia-jetpack
 ,
 }:
 let
-  majorVersion = {
+  inherit (nvidia-jetpack)
+    buildFromDebs
+    debs
+    l4t-3d-core
+    l4t-core
+    l4t-cupva
+    l4t-multimedia
+    l4tMajorMinorPatchVersion
+    ;
+
+  majorVersion = lib.getAttr (lib.versions.major l4tMajorMinorPatchVersion) {
     "35" = "2";
     "36" = "3";
-  }.${lib.versions.major l4tMajorMinorPatchVersion};
+  };
 in
 buildFromDebs {
   pname = "vpi${majorVersion}";

commit a5816b0bb65aba7cc44c7e0d1b57fac6ee12b5ee

overlay: JetPack CUDA extensions should only apply when using JetPack
The CUDA extension was erroneously applied unconditionally. It should
only apply when using the JetPack CUDA package sets.

Fixes: d4efffc357e0 ("cuda: Introduce L4T cuda-extensions")

overlay.nix
@@ -146,7 +146,8 @@ in
       prev.opencv;
 
   _cuda = prev._cuda.extend (_: prev: recursiveUpdate prev {
-    extensions = prev.extensions ++ [ (final.callPackage ./pkgs/cuda-extensions { }) ];
+    extensions = prev.extensions
+      ++ final.lib.optional useJetPackCudaPackageSet (final.callPackage ./pkgs/cuda-extensions { });
 
     # Update _cuda's database with an entry allowing Orin on CUDA 11.4.
     # NOTE: This can be removed when the minimum supported Nixpkgs version is 25.11,