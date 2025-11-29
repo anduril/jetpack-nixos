ad7e11f4f52b5aa9a5fa9c1baea013c14abc6197
Connor Baker <ConnorBaker01@gmail.com>
2025-11-28 19:04:29 -0500

    wip: re-use as much of upstream as possible
    Signed-off-by: Connor Baker <ConnorBaker01@gmail.com>
    Signed-off-by: Connor Baker <cbaker2@anduril.com>

--- pkgs/cuda-packages/libcusparse/fixup.nix
+++
@@ -1,6 +0,0 @@
-# NOTE: All fixups must be at least binary functions to avoid callPackage adding override attributes.
-{ lib, libnvjitlink ? null, cudaAtLeast }:
-prevAttrs: {
-  buildInputs = prevAttrs.buildInputs or [ ]
-    ++ lib.lists.optionals (cudaAtLeast "12") [ (lib.getLib libnvjitlink) ];
-}
