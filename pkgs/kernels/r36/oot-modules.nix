e034e70aba136c6fe75122472b83a10044c26705
Sean Miller <smiller@anduril.com>
2025-12-15 10:24:23 -0800

    Copy all generated conftest headers
    The conftest header actually includes quite a few other headers now:
    
    include "conftest/headers.h"
    include "conftest/functions.h"
    include "conftest/generic.h"
    include "conftest/macros.h"
    include "conftest/symbols.h"
    include "conftest/types.h"
    
    lets make sure we don't miss anything and include everything that is generated.

--- pkgs/kernels/r36/oot-modules.nix
+++ pkgs/kernels/r36/oot-modules.nix
@@ -95,7 +95,7 @@ stdenv.mkDerivation {
     cat **/Module.symvers > $dev/Module.symvers
 
     mkdir -p $dev/include/nvidia
-    install -m 0644 out/nvidia-conftest/nvidia/conftest.h $dev/include/nvidia/
+    cp -r out/nvidia-conftest/nvidia/* $dev/include/nvidia/
   '';
 
   outputs = [
