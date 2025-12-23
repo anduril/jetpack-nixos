anduril/jetpack-nixos

commit 4dfea63a39e3a5d0c8119471f4601248ca20313a

l4t-3d-core: Use appendRunpaths
Complete the TODO: Replace this with appendRunpaths which is available
in 23.11

pkgs/l4t/l4t-3d-core.nix
@@ -52,24 +52,5 @@ buildFromDebs {
     done
   '';
 
-  # We append a postFixupHook since we need to have this happen after
-  # autoPatchelfHook, which itself also runs as a postFixupHook
-  # TODO: Replace this with appendRunpaths which is available in 23.11
-  preFixup = ''
-    postFixupHooks+=('
-      # ls to filter out libnvidia-vulkan-producer.so, which is only present in r35
-      patchelf --add-rpath ${lib.makeLibraryPath [ libglvnd ]} \
-        $out/lib/libEGL_nvidia.so.0 \
-        $out/lib/libGLX_nvidia.so.0 \
-        $(ls $out/lib/libnvidia-vulkan-producer.so)
-
-      patchelf --add-rpath ${lib.makeLibraryPath (with xorg; [ libX11 libXext libxcb ])} \
-        $out/lib/libGLX_nvidia.so.0 \
-        $out/lib/libnvidia-glsi.so.*
-
-      for lib in $(find "$out/lib" -name "*.so*"); do
-        patchelf $lib --add-rpath $out/lib
-      done
-    ')
-  '';
+  appendRunpaths = [ "${placeholder "out"}/lib" libglvnd xorg.libX11 xorg.libXext xorg.libxcb ];
 }

commit cdae4b7f43b419fa5df728ff6c00244ee66bd7e9

l4t-3d-core: Fix the appendRunpaths attribute
appendRunpaths expects the full path you want to append, and doesn't do
any further manipulation. We intended to add the library path of
libglvnd and others, but ended up only adding the base $out output.

Fixes: 4dfea63a39e3 ("l4t-3d-core: Use appendRunpaths")

pkgs/l4t/l4t-3d-core.nix
@@ -52,5 +52,5 @@ buildFromDebs {
     done
   '';
 
-  appendRunpaths = [ "${placeholder "out"}/lib" libglvnd xorg.libX11 xorg.libXext xorg.libxcb ];
+  appendRunpaths = [ "${placeholder "out"}/lib" ] ++ builtins.map (p: (lib.getLib p) + "/lib") [ libglvnd xorg.libX11 xorg.libXext xorg.libxcb ];
 }