c9681551beaa39f351197f8e4c815e9df392c1f5
dperezzoghbi <dperezzoghbi@anduril.com>
2026-01-27 20:04:33 -0800

    optee: Fix xtest failures 1008 and 1033
    xtest -t regression 1008
    xtest -t regression 1033
    
    The above test cases were failing due to not being able to open the
    corrupt TA for test 1008 (searches locally on TA_DIR make flag), and
    it was missing the plugin needed for 1033.
    
    This change formalizes the TA and the Plugin paths and adds them to
    the optee.supplicant service's path.

--- pkgs/optee/opteeXtest.nix
+++ pkgs/optee/opteeXtest.nix
@@ -8,8 +8,9 @@
 , opteeClient
 , taDevKit
 , l4tAtLeast
+, symlinkJoin
 }:
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "optee_xtest";
   version = l4tMajorMinorPatchVersion;
 
@@ -25,11 +26,28 @@ stdenv.mkDerivation {
   postPatch = ''
     patchShebangs --build $(find optee/optee_test -type d -name scripts -printf '%p ')
   '';
+
+  # Use passthru + symlinkJoin instead of multiple outputs to avoid cyclic
+  # dependencies: xtest binary needs to reference TAs, and TAs contain
+  # references to $out in metadata/symbols (can't strip - they're signed).
+  passthru =
+    let
+      path = dir: "${lib.getOutput "out" finalAttrs.finalPackage }/${dir}";
+      package = name: symlinkJoin {
+        inherit name;
+        paths = [(path name)];
+      };
+    in {
+      tas = package "tas";
+      plugins = package "plugins";
+    };
+
   makeFlags = [
     "-C optee/optee_test"
     "CROSS_COMPILE=${stdenv.cc.targetPrefix}"
     "OPTEE_CLIENT_EXPORT=${opteeClient}"
     "TA_DEV_KIT_DIR=${taDevKit}/export-ta_arm64"
+    "TA_DIR=$(out)/tas" # xtest needs to manually load corrupt TA for test 1008
     "O=$(PWD)/out"
   ] ++ lib.optionals (l4tAtLeast "36") [
     "WITH_OPENSSL=n"
@@ -38,8 +56,10 @@ stdenv.mkDerivation {
     runHook preInstall
 
     install -Dm 755 ./out/xtest/xtest $out/bin/xtest
-    find ./out -name "*.ta" -exec cp {} $out \;
+
+    find ./out -name "*.ta" -print0 | xargs -0 install -Dm 755 -t $out/tas
+    find ./out -name "*.plugin" -print0 | xargs -0 install -Dm 755 -t $out/plugins
 
     runHook postInstall
   '';
-}
+})
