2a03a285bf78834ae5edb30e514b0683e7d04c2b
dperezzoghbi <dperezzoghbi@anduril.com>
2026-02-10 14:21:03 -0800

    optee: Create multiple outputs for xtest TAs and Plugins
    TAs and Plugins are needed for tee-supplicant. The xtest binary does
    require a path to the TAs for loading in the TA_DIR=$(tas) make flag,
    which is hard coded into the binary. This is a true runtime
    dependency, which nix finds with strings.
    
    We need to ensure that the TAs do not have a back reference to $out.
    We cannot use remove-references-to or nuke-refs in the installPhase as
    is typical, because the TAs are signed in the buildPhase. To achive
    this, a 'hook' is added into make, that creates a preSign phase, which
    calls nuke-refsa before signing.
    
    All xtest tests are passing.

--- pkgs/optee/default.nix
+++ pkgs/optee/default.nix
@@ -14,7 +14,6 @@
 , gitRepos
 , uefi-firmware
 , openssl
-, symlinkJoin
 }:
 
 let
@@ -54,8 +53,17 @@ let
     version = l4tMajorMinorPatchVersion;
     src = nvopteeSrc;
     patches = [ ./0001-GCC-15-compile-fix.patch ];
+
+    # Multiple outputs: xtest binary, TAs, and plugins
+    outputs = [
+      "out"
+      "tas"
+      "plugins"
+    ];
+
     nativeBuildInputs = [
       (buildPackages.python3.withPackages (p: [ p.cryptography ]))
+      nukeReferences
     ] ++ lib.optionals (l4tOlder "36") [ openssl ];
 
     buildInputs = [ ] ++ lib.optionals (l4tOlder "36") [ openssl ];
@@ -64,38 +72,53 @@ let
       patchShebangs --build $(find optee/optee_test -type d -name scripts -printf '%p ')
     '';
 
-    # Use passthru + symlinkJoin instead of multiple outputs to avoid cyclic
-    # dependencies: xtest binary needs to reference TAs, and TAs contain
-    # references to $out in metadata/symbols (can't strip - they're signed).
-    passthru =
-      let
-        path = dir: "${lib.getOutput "out" finalAttrs.finalPackage }/${dir}";
-        package = name: symlinkJoin {
-          inherit name;
-          paths = [(path name)];
-        };
-      in {
-        tas = package "tas";
-        plugins = package "plugins";
-      };
-
     makeFlags = [
       "-C optee/optee_test"
       "CROSS_COMPILE=${stdenv.cc.targetPrefix}"
       "OPTEE_CLIENT_EXPORT=${opteeClient}"
       "TA_DEV_KIT_DIR=${buildOpteeTaDevKit args}/export-ta_arm64"
-      "TA_DIR=$(out)/tas" # xtest needs to manually load corrupt TA for test 1008
+      "TA_DIR=$(tas)" # xtest needs to manually load corrupt TA for test 1008
       "O=$(PWD)/out"
     ] ++ lib.optionals (l4tAtLeast "36") [
       "WITH_OPENSSL=n"
     ];
+
+    # We need to nuke-refs here because $tas/<uuid>.ta will retain a string
+    # reference to $out/lib (and glibc/lib) on the nix-store. We need to break
+    # this reference in order to split $out and $ta refs since $out/bin/xtest
+    # refers to the $tas location for runtime loading.
+    # The reason why the $out/lib reference is included in the TAs in the first
+    # place is during the linking phase, some TAs can statically link to
+    # eachother. This $out/lib path remains in the RPATH/RUNPATH for library
+    # searching metadata. The existing objcopy --strip-unneeded does not strip
+    # this unformation, despite it not being necessary for our TAs.
+    preBuild =
+      let
+        taPreSign = ''
+          cp \$input \$output
+          nuke-refs \$output
+        '';
+      in ''
+        cat > ta-pre-sign.sh <<EOF
+        #!/bin/sh
+        set -euo pipefail
+
+        input="\$1"
+        output="\$2"
+
+        ${taPreSign}
+        EOF
+        chmod +x ta-pre-sign.sh
+        makeFlagsArray+=(NIX_TA_PRE_SIGN_HOOK="$PWD/ta-pre-sign.sh")
+      '';
+
     installPhase = ''
       runHook preInstall
 
       install -Dm 755 ./out/xtest/xtest $out/bin/xtest
 
-      find ./out -name "*.ta" -print0 | xargs -0 install -Dm 755 -t $out/tas
-      find ./out -name "*.plugin" -print0 | xargs -0 install -Dm 755 -t $out/plugins
+      find ./out -name "*.ta" -print0 | xargs -0 install -Dm 755 -t $tas
+      find ./out -name "*.plugin" -print0 | xargs -0 install -Dm 755 -t $plugins
 
       runHook postInstall
     '';
@@ -153,7 +176,10 @@ let
       inherit pname;
       version = l4tMajorMinorPatchVersion;
       src = nvopteeSrc;
-      patches = opteePatches ++ [ ./remove-force-log-level.diff ];
+      patches = opteePatches ++ [
+        ./remove-force-log-level.diff
+        ./0003-Add-post-strip-hook.patch
+      ];
       postPatch = ''
         patchShebangs $(find optee/optee_os -type d -name scripts -printf '%p ')
       '';
