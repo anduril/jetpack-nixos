fd71b892165c0893e6b37c3dcc81d6c0af84b131
dperezzoghbi <dperezzoghbi@anduril.com>
2026-03-30 16:22:02 -0700

    optee: Refactor ta-pre-sign hook to use writeShellApplication
    Replace the manual shell script creation in preBuild with writeShellApplication
    for the TA pre-sign hook. This is more idiomatic in Nix and provides better
    error handling and dependency management through runtimeInputs.
    
    The functionality remains unchanged - the hook still copies the TA and strips
    store references before signing.

--- pkgs/optee/opteeXtest.nix
+++ pkgs/optee/opteeXtest.nix
@@ -9,6 +9,7 @@
 , opteeClient
 , taDevKit
 , l4tAtLeast
+, writeShellApplication
 }:
 stdenv.mkDerivation {
   pname = "optee_xtest";
@@ -27,7 +28,6 @@ stdenv.mkDerivation {
 
   nativeBuildInputs = [
     (buildPackages.python3.withPackages (p: [ p.cryptography ]))
-    nukeReferences
   ] ++ lib.optionals (l4tOlder "36") [ openssl ];
 
   buildInputs = [ ] ++ lib.optionals (l4tOlder "36") [ openssl ];
@@ -36,16 +36,41 @@ stdenv.mkDerivation {
     patchShebangs --build $(find optee/optee_test -type d -name scripts -printf '%p ')
   '';
 
-  makeFlags = [
-    "-C optee/optee_test"
-    "CROSS_COMPILE=${stdenv.cc.targetPrefix}"
-    "OPTEE_CLIENT_EXPORT=${opteeClient}"
-    "TA_DEV_KIT_DIR=${taDevKit}/export-ta_arm64"
-    "TA_DIR=$(tas)" # xtest needs to manually load corrupt TA for test 1008
-    "O=$(PWD)/out"
-  ] ++ lib.optionals (l4tAtLeast "36") [
-    "WITH_OPENSSL=n"
-  ];
+  makeFlags =
+    let
+      # We need to nuke-refs here because $tas/<uuid>.ta will retain a string
+      # reference to $out/lib (and glibc/lib) on the nix-store. We need to break
+      # this reference in order to split $out and $ta refs since $out/bin/xtest
+      # refers to the $tas location for runtime loading.
+      # The reason why the $out/lib reference is included in the TAs in the first
+      # place is during the linking phase, some TAs can statically link to
+      # eachother. This $out/lib path remains in the RPATH/RUNPATH for library
+      # searching metadata. The existing objcopy --strip-unneeded does not strip
+      # this unformation, despite it not being necessary for our TAs.
+      # We need this to be done during the build phase since we cannot nuke-refs
+      # after signing, as it will make verification of the signed TA fail.
+      taPreSign = writeShellApplication {
+        name = "ta-pre-sign.sh";
+        runtimeInputs = [ nukeReferences ];
+        text = ''
+          input="$1"
+          output="$2"
+          cp "$input" "$output"
+          nuke-refs "$output"
+        '';
+      };
+    in
+    [
+      "-C optee/optee_test"
+      "CROSS_COMPILE=${stdenv.cc.targetPrefix}"
+      "OPTEE_CLIENT_EXPORT=${opteeClient}"
+      "TA_DEV_KIT_DIR=${taDevKit}/export-ta_arm64"
+      "TA_DIR=$(tas)" # xtest needs to manually load corrupt TA for test 1008
+      "O=$(PWD)/out"
+      "NIX_TA_PRE_SIGN_HOOK=${lib.getExe taPreSign}"
+    ] ++ lib.optionals (l4tAtLeast "36") [
+      "WITH_OPENSSL=n"
+    ];
   installPhase = ''
     runHook preInstall
 
@@ -56,35 +81,4 @@ stdenv.mkDerivation {
 
     runHook postInstall
   '';
-
-  # We need to nuke-refs here because $tas/<uuid>.ta will retain a string
-  # reference to $out/lib (and glibc/lib) on the nix-store. We need to break
-  # this reference in order to split $out and $ta refs since $out/bin/xtest
-  # refers to the $tas location for runtime loading.
-  # The reason why the $out/lib reference is included in the TAs in the first
-  # place is during the linking phase, some TAs can statically link to
-  # eachother. This $out/lib path remains in the RPATH/RUNPATH for library
-  # searching metadata. The existing objcopy --strip-unneeded does not strip
-  # this unformation, despite it not being necessary for our TAs.
-  preBuild =
-    let
-      taPreSign = ''
-        cp \$input \$output
-        nuke-refs \$output
-      '';
-    in
-    ''
-      cat > ta-pre-sign.sh <<EOF
-      #!/bin/sh
-      set -euo pipefail
-
-      input="\$1"
-      output="\$2"
-
-      ${taPreSign}
-      EOF
-      chmod +x ta-pre-sign.sh
-      makeFlagsArray+=(NIX_TA_PRE_SIGN_HOOK="$PWD/ta-pre-sign.sh")
-    '';
-
 }
