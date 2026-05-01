786198256d509c5dfa50ca2ea53a549ca98f2dd8
Hlib Hraif <hlib.hraif@protonmail.com>
2026-05-01 21:58:09 +0200

    Fix the infinite recursion in taDevKit

--- pkgs/optee/taDevKit.nix
+++ pkgs/optee/taDevKit.nix
@@ -2,5 +2,7 @@
 optee-os.overrideAttrs (finalAttrs: {
   pname = "optee-ta-dev-kit";
   uefi-firmware = null;
+  ftpmHelperTa = null;
+  msTpm20RefTa = null;
   makeFlags = finalAttrs.makeFlags or [ ] ++ [ "ta_dev_kit" ];
 })
